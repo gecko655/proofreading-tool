cbfd3f3ffb21ff54b79cc5b923e3a28ba760acaa
ayamamori <akihiro.yamamori@mixi.co.jp>
2021-04-08 12:47:20 +0900

    proofreading-toolのfirst commit

---
+++ assets/js/entries/index.js
@@ -0,0 +1,62 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Vue from 'vue';
+import 'bootstrap';
+
+import Mark from 'mark.js';
+
+import { ipcRenderer } from 'electron';
+
+Vue.directive('highlight-range', (el, binding) => {
+  const markInstance = new Mark(el);
+  const indices = binding.arg.ViolatedIndices;
+  indices.forEach((index) => {
+    markInstance.markRanges([{ start: index.Start, length: index.Length }]);
+  });
+});
+
+const vue = new Vue({
+  data: {
+    ok: false,
+    proofreadResults: [],
+    errorMsg: '',
+    isUploading: false,
+    fileName: '',
+  },
+  methods: {
+    onDrop(event) {
+      if (
+        !event ||
+        !event.dataTransfer ||
+        event.dataTransfer.files.length === 0
+      ) {
+        return;
+      }
+      const file = event.dataTransfer.files[0];
+      this.ok = false;
+      this.proofreadResults = [];
+      this.fileName = '';
+      this.errorMsg = '';
+      this.isUploading = true;
+      ipcRenderer.invoke('proofread', file.path).then((proofreadResult) => {
+        console.log(proofreadResult);
+        if (proofreadResult.errorMsg != null) {
+          this.errorMsg = proofreadResult.errorMsg;
+          this.isUploading = false;
+          return;
+        }
+        this.ok = proofreadResult.ok;
+        this.proofreadResults = proofreadResult.results;
+        this.fileName = proofreadResult.fileName;
+        this.isUploading = false;
+      });
+    },
+    hasFileName() {
+      return this.fileName.length > 0;
+    },
+    openTextlintConfig() {
+      ipcRenderer.send('openTextlintConfig');
+    },
+  },
+});
+vue.$mount('#vue');
